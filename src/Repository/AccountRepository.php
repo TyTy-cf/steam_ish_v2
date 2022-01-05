9d476d9a59066fc211a71c5f1a21b0126364b3f2
Kevin Tourret <tarpion@toto.fr>
2022-01-05 10:24:11 +0100

    abort mission

--- src/Repository/AccountRepository.php
+++ src/Repository/AccountRepository.php
@@ -37,8 +37,6 @@ class AccountRepository extends ServiceEntityRepository
             ->select('account', 'libraries')
             ->join('account.libraries', 'libraries')
             ->orderBy($order, 'ASC')
-            ->groupBy('account.name')
-            ->setMaxResults(25)
             ->getQuery()
             ->getResult()
         ;
